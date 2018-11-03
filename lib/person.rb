5ed362f80a57ea24d6290e919077634d0d21f91b
Saad Malik <saad.t.malik@gmail.com>
2018-11-03 16:41:42 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+class Person
+  def intialize (name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  def name=(new_name)
+    @name=new_name
+  end
+end
+
+class Person
+  def intialize (job)
+    @job = job
+  end
+  def job
+    @job
+  end
+  def job=(new_job)
+    @job=new_job
+  end
+end
