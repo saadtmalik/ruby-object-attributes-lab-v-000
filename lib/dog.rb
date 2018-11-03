e74f1d262f153a1b3e858238ac9796d0dbb5d207
Saad Malik <saad.t.malik@gmail.com>
2018-11-03 16:39:57 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,23 @@
 class Dog
-  def intialize (name, breed)
+  def intialize (name)
     @name = name
-    @breed = breed
   end
-
-  def name=(new_name_and_breed)
-    name, breed =new_name_and_breed.split
-    @name = name
-    @breed = breed
-  end
-
   def name
-    "#{@name} #{@breed}".strip
+    @name
+  end
+  def name=(new_name)
+    @name=new_name
+  end
+end
+
+class Dog
+  def intialize (breed)
+    @breed = breed
+  end
+  def breed
+    @breed
+  end
+  def breed=(new_breed)
+    @breed=new_breed
   end
 end
