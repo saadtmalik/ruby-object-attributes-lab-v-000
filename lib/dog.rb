9721b48d1b999c570a13e4a65c7586f24e523df1
Saad Malik <saad.t.malik@gmail.com>
2018-11-03 16:28:56 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,25 @@
 class Dog
+
   def intialize (name, breed)
     @name = name
     @breed = breed
   end
-  
+
   def name
     @name
+  end
+
+  def name=(new_name)
+    name=new_name
   end
 
   def breed
     @breed
   end
+
+  def breed=(new_breed)
+    name=new_breed
+  end
 end
 
-fido = Dog.new("Fido", "Beagle")
+fido=Dog.new("Fido", "Beagle")
